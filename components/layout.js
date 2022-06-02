886471c4b793e22096fdb04d35b2e96985d6575e
fqishuai <rawlinsfeng@163.com>
2022-06-02 20:11:31 +0800

    feat: 调用搭建的nestjs服务提供的接口

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Name666';
+const name = 'Rawlins Feng';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,52 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import indexStyles from './index.module.css'
+// import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  // const allPostsData = getSortedPostsData();
+  // return {
+  //   props: {
+  //     allPostsData,
+  //   },
+  // };
+  const res = await fetch('https://nest-serverless.vercel.app/user/2');
+  const resJson = await res.json();
+  console.log('resJson::', resJson)
   return {
     props: {
-      allPostsData,
+      allPostsData: [{id:1,title:resJson.data || resJson.message}],
     },
   };
 }
 
 export default function Home({ allPostsData }) {
+
+  const handleCreateUser = () => {
+    fetch('https://nest-serverless.vercel.app/user/create',{
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'root',
+        age: 20,
+      })
+    }).then(res => {
+      res.json().then(result => {
+        console.log('result::', result)
+      })
+    })
+  }
+
   return (
     <Layout home>
       <Head>
         <title>{'Home'}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <div className={indexStyles.flexContainer}>
+          <p><a href="https://nextjs.org/learn">Next.js tutorial</a></p>
+          <div className={indexStyles.click} onClick={handleCreateUser}>点击新增用户1</div>
+        </div>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -34,20 +57,6 @@ export default function Home({ allPostsData }) {
                <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
               <br />
               {id}
               <br />
